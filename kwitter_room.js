b0057b2a2b9e7f40d6f93f5c84162c8d6f5650a3
The Muffin Man <52115365+Rameen2010@users.noreply.github.com>
2023-08-22 15:56:02 -0400

    Add files via upload

---
+++ kwitter_room.js
@@ -0,0 +1,42 @@
+
+var firebaseConfig = {
+      apiKey: "AIzaSyA1guRpwRgJBvYiV55ygVz551nAwYpGBBM",
+      authDomain: "lets-chat-a819c.firebaseapp.com",
+      databaseURL: "https://lets-chat-a819c-default-rtdb.firebaseio.com",
+      projectId: "lets-chat-a819c",
+      storageBucket: "lets-chat-a819c.appspot.com",
+      messagingSenderId: "613572619572",
+      appId: "1:613572619572:web:522a97cea698d7b7520961"
+    };
+    
+    // Initialize Firebase
+    firebase.initializeApp(firebaseConfig);
+    user_name = localStorage.getItem("user_name");
+    document.getElementById("user_name").innerHTML = "Welcome "+ user_name;
+function getData() {firebase.database().ref("/").on('value', function(snapshot) {document.getElementById("output").innerHTML = "";snapshot.forEach(function(childSnapshot) {childKey  = childSnapshot.key;
+       Room_names = childKey;
+      //Start code
+console.log("Room Name -" + Room_names);
+row = "<div class='room_name' id="+Room_names+" onclick='redirectToRoomName(this.id)' >#" + Room_names + "</div><hr>";
+document.getElementById("output").innerHTML += row;
+      //End code
+      });});}
+getData();
+function addRoom()
+{
+      room_name = document.getElementById("room_name").value;
+
+      firebase.database().ref("/").child(room_name).update({
+            purpose : "adding room name"
+      });
+
+      localStorage.setItem("room_name", room_name);
+
+      window.location = "kwitter_page.html";
+}
+function redirectToRoomName(name)
+{
+      console.log(name);
+      localStorage.setItem("room_name", name);
+      window.location = "kwitter_page.html";
+}
